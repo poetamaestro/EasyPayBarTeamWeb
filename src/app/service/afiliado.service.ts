43374a6d11b8fb81e5dd2032e4fb6e3884291512
francisrubio06 <francisrubio06@gmail.com>
2017-05-31 21:38:20 -0500

    Raiz de afiliado es el UID

--- src/app/service/afiliado.service.ts
+++ src/app/service/afiliado.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { Afiliado } from './../typeScript/afiliado';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class AfiliadoService {
 
-afiliados : FirebaseListObservable<Afiliado[]>;
+afiliados: FirebaseListObservable<Afiliado[]>;
 afiliado: Afiliado = new Afiliado();
 
   constructor(private db: AngularFireDatabase) {  }
 
-  getAfiliados( id): FirebaseListObservable<Afiliado[]>{
-  	return this.db.list('/proveedor/'+id+'/afiliados');
+  getAfiliados( id): FirebaseListObservable<Afiliado[]> {
+    return this.db.list('/proveedor/' + id + '/afiliados');
   }
-  ActualizarSaldo(id: number , saldo: string ,idAfiliado: number) {
-    const consultaAfiliadoId = this.db.object('/proveedor/'+id+'/afiliados/'+idAfiliado);
+  ActualizarSaldo(id: number , saldo: string , idAfiliado: number) {
+    const consultaAfiliadoId = this.db.object('/proveedor/' + id + '/afiliados/' + idAfiliado);
     consultaAfiliadoId.update({ saldo: saldo });
   }
-  agregarAfiliado(nuevoAfiliado: Afiliado, id){
-    this.db.list('/proveedor/'+id+'/afiliados').push(nuevoAfiliado);
-
-
+  agregarAfiliado(nuevoAfiliado: Afiliado, id) {
+    firebase.database().ref('/proveedor/' + id + '/afiliados/' + nuevoAfiliado.codigoQR).set({
+      fechaAfiliacion: nuevoAfiliado.fechaAfiliacion,
+      key: nuevoAfiliado.codigoQR,
+      nombre: nuevoAfiliado.nombre,
+      saldo: nuevoAfiliado.saldo,
+    });
   }
-
 }
