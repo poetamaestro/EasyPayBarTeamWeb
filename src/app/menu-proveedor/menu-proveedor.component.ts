d9be9e3631219070e1e3e30353de66680c74e290
SebasGalarza <andyxd1994@hotmail.com>
2017-06-14 23:43:38 -0500

    Menu  por funciones y botones en afiliados
    Se ha realizado el menu pro funciones, y mejoras en afiliados en un solo
    estandar por botones

--- src/app/menu-proveedor/menu-proveedor.component.ts
+++ src/app/menu-proveedor/menu-proveedor.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+//import { Router } from '@angular/router';
+//import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { Router , RouterLink} from '@angular/router';
+import { AngularFire} from 'angularfire2';
 @Component({
   selector: 'app-menu-proveedor',
   templateUrl: './menu-proveedor.component.html',
@@ -10,14 +12,62 @@ export class MenuProveedorComponent implements OnInit {
 
   public isCollapsed: boolean = true;
 
+  administrador = false;
+  proveedor = false;
   user : any
-  pictureUrl : any;
-  isLogin : boolean = false;
+  //pictureUrl : any;
+  //isLogin : boolean = false;
   id: string;
 
   constructor(public af: AngularFire,private router: Router) {
-    if(router.url == '/login') {
-      this.isLogin = true;
+    //if(router.url == '/login') {
+      //this.isLogin = true;
+
+
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+
+        this.filtro(af, auth);
+
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+  filtro(af , auth) {
+
+
+const queryObservable = af.database.list('/cliente', {
+      query: {
+        orderByChild: 'codigoQR',
+        equalTo: auth.uid
+      }
+    });
+
+// subscribe to changes
+    queryObservable.subscribe(queriedItems => {
+      this.administrador =  queriedItems[0].admin;
+      this.proveedor = queriedItems[0].proveedor;
+    });
+
+    const queryObservableProveedor = af.database.list('/proveedor', {
+      query: {
+        orderByChild: 'codigoQR',
+        equalTo: auth.uid
+      }
+    });
+
+    queryObservableProveedor.subscribe(queriedItems => {
+      if (queriedItems.length > 0) {
+        this.id = queriedItems[0].$key;
+      }
+    });
+
+  }
+
+
+    /*
 
     }
 
@@ -53,5 +103,5 @@ export class MenuProveedorComponent implements OnInit {
   ngOnInit() {
 
   }
-
+*/
 }
